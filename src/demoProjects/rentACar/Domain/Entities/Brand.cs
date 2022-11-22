78beb70a4d6bb5747130f471fdcce4acc7394db3
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-22 15:09:28 +0300

    Entity added with Constructors

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -7,17 +7,15 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Brand:Entity
+    public class Brand : Entity
     {
         public string Name { get; set; }
-
-        public virtual ICollection<Model> Models { get; set; }
 
         public Brand()
         {
         }
 
-        public Brand(int id, string name):this()
+        public Brand(int id, string name) : this()
         {
             Id = id;
             Name = name;

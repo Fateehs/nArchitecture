e567f5cea3421623137322cd506df50b4215aeaf
Fatih Enes Selvi <benzer1010@gmail.com>
2022-11-29 18:30:36 +0300

    Business Rule activated on Execute of Assembly

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Core.Application.Pipelines.Authorization;
+﻿using Application.Features.Brands.Rules;
+using Core.Application.Pipelines.Authorization;
 using Core.Application.Pipelines.Caching;
 using Core.Application.Pipelines.Logging;
 using Core.Application.Pipelines.Validation;
@@ -21,7 +22,7 @@ namespace Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
-            //services.AddScoped<SomeFeatureEntityBusinessRules>();
+            services.AddScoped<BrandBusinessRules>();
 
             //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
